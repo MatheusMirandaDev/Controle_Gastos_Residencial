74ff3ab9c44b76b95c6208900e7eb21603892358
MatheusMirandaDev <matheusmiranda.batista@gmail.com>
2025-03-18 16:53:19 -0300

    Melhorando a página de home

--- controle-gastos-frontend/src/pages/Home/Home.jsx
+++ controle-gastos-frontend/src/pages/Home/Home.jsx
@@ -11,12 +11,11 @@ const Home = () => {
       {/* Descrição geral do funcionamento do sistema */}
       <p>
         O Sistema de Gestão de Gastos Residenciais foi desenvolvido para ajudar
-        no controle das finanças domésticas de maneira simples e eficiente. Ele
-        permite o gerenciamento de despesas e receitas de cada pessoa da casa,
+        no controle financeiro doméstico de maneira simples e eficiente. Ele
+        permite o gerenciamento de receitas e despesas de cada pessoa da casa,
         auxiliando no controle do saldo geral.
       </p>
       <section>
-        {" "}
         {/* Seção que descreve as funcionalidades do sistema */}
         <h2>Funcionalidades</h2>
         {/* Caixa de funcionalidades: Cadastro de Pessoas */}
@@ -24,13 +23,12 @@ const Home = () => {
           <h3>Cadastro de Pessoas</h3> {/* Título da funcionalidade */}
           <p>
             Crie, visualize, atualize e remova registros de pessoas no sistema.
-          </p>{" "}
+          </p>
           {/* Explicação do que a funcionalidade faz */}
           <h6>
-            {" "}
-            {/* Explicação sobre o comportamento ao excluir uma pessoa */}* Ao
-            excluir uma pessoa, todas as suas transações serão excluídas
-            automaticamente.
+            {/* Explicação sobre o comportamento ao excluir uma pessoa */}
+            *Atenção: Ao excluir uma pessoa, todas as suas transações serão
+            excluídas automaticamente.
           </h6>
         </div>
         {/* Caixa de funcionalidades: Cadastro de Transações */}
@@ -41,18 +39,18 @@ const Home = () => {
             sistema.
           </p>
           <h6>
-            {" "}
             {/* Restrição para menores de 18 anos não poderem registrar receitas */}
-            * Pessoas menores de 18 anos não podem registrar transações do tipo
-            ´´receita``.
+            *Atenção: Pessoas menores de 18 anos não podem registrar transações
+            do tipo
+            {' "receita"'}.
           </h6>
         </div>
         {/* Caixa de funcionalidades: Consulta de Totais */}
         <div className="feature-box">
           <h3>Consulta de Totais</h3>
           <p>
-            Visualize o total de receitas, despesas e o saldo individual de cada
-            pessoa, além do saldo geral da residência.
+            Visualize o total de receitas, despesas e o saldo individual, além
+            do saldo geral da residência.
           </p>
         </div>
         <h2>Como Usar o Sistema?</h2> {/* Subtítulo com instruções de uso */}
@@ -61,32 +59,33 @@ const Home = () => {
         <div className="feature-box">
           <h3>1º - Cadastro de Pessoas</h3>
           <p>
-            Adicione uma pessoa ao sistema. No topo da tela, há uma barra de
-            navegação. Clique em <em>´´Cadastro de Pessoas``</em>. Nessa página,
-            você poderá visualizar todas as pessoas cadastradas no sistema e
-            terá a opção de adicionar ou excluir registros.
+            - Acesse a página <strong>{' "Cadastro de Pessoas"'}</strong> pela
+            barra de navegação.{" "}
+          </p>
+          <p>
+            - Visualize todas as pessoas cadastradas e adicione ou remova
+            registros conforme necessário.
           </p>
         </div>
         {/* Passo 2: Cadastro de Transações */}
         <div className="feature-box">
           <h3>2º - Cadastro de Transações</h3>
           <p>
-            Cadastre as transações (despesas ou receitas) para cada pessoa,
-            conforme o tipo de gasto. Dê uma descrição para a transação. No topo
-            da tela, há uma barra de navegação.
-            <em>´´Cadastro de Transações``</em>. Na aba de transações, você
-            poderá visualizar todas as transações registradas e seus respectivos
-            responsáveis ao final da tela, embaixo do formulario.
+            - Na página de.
+            <strong>{' "Cadastro de Transações"'}</strong>, registre despesas ou
+            receitas para cada pessoa.
+          </p>
+          <p>
+            - No final da tela, você poderá visualizar as transações registradas
+            e seus responsáveis.
           </p>
         </div>
         {/* Passo 3: Consulta de Totais */}
         <div className="feature-box">
           <h3>3º - Consulta de Totais</h3>
           <p>
-            Consulte os totais de receitas, despesas e saldo de cada pessoa,
-            além do total geral da residência. Para acessar essa funcionalidade,
-            vá até a barra de navegação no topo da tela e clique em
-            <em>´´Consulta de Totais``</em>.
+            - Acesse a página de <strong>{' "Consulta de Totais"'}</strong> para
+            visualizar o saldo individual e o total geral da residência.
           </p>
         </div>
       </section>
